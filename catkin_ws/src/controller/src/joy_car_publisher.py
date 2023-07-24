4da73c3d530341e8ffbdd75d806244e282af42bb
Himadri Roy <himadri.roy355@gmail.com>
2023-07-24 15:07:09 -0400

    tweaks and added safety node

--- catkin_ws/src/controller/src/joy_car_publisher.py
+++ catkin_ws/src/controller/src/joy_car_publisher.py
@@ -3,42 +3,59 @@
 import rospy
 from sensor_msgs.msg import Joy
 from ackermann_msgs.msg import AckermannDriveStamped
-from geometry_msgs.msg import TwistStamped
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Int32, Bool
 import math
 
 
 class Joystick:
     def __init__(self):
         self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_callback)
-        self.joy_pub = rospy.Publisher("/cmd_vel_", TwistStamped, queue_size=1)
+        self.joy_pub = rospy.Publisher("/joy_drive", AckermannDriveStamped, queue_size=1)
+        self.auto_control_pub = rospy.Publisher("/auto_flag", Int32, queue_size=1)
+        self.enable_car_bool = rospy.Publisher("/en_car", Bool, queue_size=1)
+        
         self.last_joy = None
-        self.rate = rospy.Rate(20)
+        self.rate = rospy.Rate(40)
         self.header = Header()
-        self.twist_msg = TwistStamped()
+        
+        self.ackermann_msg = AckermannDriveStamped()
+        self.auto_control_msg = Int32()
         self.throttle = 0
         self.steering_ang = 0
+        self.enable_car_bool_msg = Bool()
 
     def joy_callback(self, msg):
         self.last_joy = msg
+
         if msg.buttons[1] == 1 and msg.axes[1] < 0:
             self.throttle = msg.axes[1] * 1
         else:
             self.throttle = msg.axes[1] * 1
 
-        self.steering_ang = msg.axes[2] * math.pi/4
+        self.steering_ang = msg.axes[3] * math.pi/4
 
+        self.enable_car_bool_msg.data = True if msg.buttons[4] and msg.buttons[5] else False
+        
+        self.auto_control_msg.data = 1 if msg.axes[5] < 0 else 0
+        
+        
     def run(self):
         while not rospy.is_shutdown():
             if self.last_joy:
                 self.header.stamp = rospy.Time.now()
                 self.header.frame_id = "base_link"
-                self.twist_msg.header = self.header
-                self.twist_msg.twist.linear.x = self.throttle
-                self.twist_msg.twist.angular.z = self.steering_ang
-
-                self.joy_pub.publish(self.twist_msg)
-
+                
+                self.ackermann_msg.header = self.header
+                self.ackermann_msg.drive.speed = self.throttle
+                self.ackermann_msg.drive.steering_angle = self.steering_ang
+       
+		
+                self.joy_pub.publish(self.ackermann_msg)
+                
+                self.auto_control_pub.publish(self.auto_control_msg)
+                self.enable_car_bool.publish(self.enable_car_bool_msg)
+                
+		 
             else:
                 rospy.loginfo("No new msgs!")
             self.rate.sleep()
--- catkin_ws/src/controller/src/mux_control.py
+++ catkin_ws/src/controller/src/mux_control.py
@@ -15,6 +15,7 @@ class Mux:
         self.wf_drive_sub = rospy.Subscriber("/wf_drive", AckermannDriveStamped, self.wf_drive_cb)
         self.drive_pub = rospy.Publisher("/ackermann_cmd", AckermannDriveStamped, queue_size=1)
         self.brake_flag_sub = rospy.Subscriber("/brake_bool", Bool, self.brake_flag_cb)
+        self.en_car_bool_sub = rospy.Subscriber("/en_car", Bool, self.en_car_bool_cb)
 
         self.rate = rospy.Rate(15)
         self.wf_ackermann_msg = AckermannDriveStamped()
@@ -23,7 +24,9 @@ class Mux:
         self.header = Header()
         self.auto_flag = 0
         self.brake_flag = 0
+        self.en_car_status = 0
         self.status_flag = False
+        self.en_print_flag = "ENABLED"
 
     def auto_flag_cb(self, wf_flag_msg):
         if wf_flag_msg.data == 1:
@@ -40,37 +43,54 @@ class Mux:
     def brake_flag_cb(self, brake_flag_msg):
         self.brake_flag = brake_flag_msg.data
 
+    def en_car_bool_cb(self, en_car_msg):
+        if en_car_msg.data:
+            self.en_car_status += 1
+
     def run(self):
 
         while not rospy.is_shutdown():
 
-            if self.wf_ackermann_msg and self.joy_ackermann_msg:
-                
-                self.header.stamp = rospy.Time.now()
-                self.header.frame_id = "base_link"
-                self.wf_ackermann_msg.header = self.header
-                self.joy_ackermann_msg.header = self.header
-                self.brake_msg_.header = self.header
+            self.header.stamp = rospy.Time.now()
+            self.header.frame_id = "base_link"
+            self.wf_ackermann_msg.header = self.header
+            self.joy_ackermann_msg.header = self.header
+            self.brake_msg_.header = self.header
 
-                if not self.brake_flag:
+            if self.en_car_status > 0:  #activate car
+                if self.en_print_flag == "DISABLED":
+                    rospy.loginfo("CAR ENABLED!")
+                    self.en_print_flag = "ENABLED"
+
+                if self.wf_ackermann_msg and self.joy_ackermann_msg:
                     
-                    if self.auto_flag == 1:
-                        if not self.status_flag:
-                            rospy.loginfo("WALL FOLLOW MODE")
-                            self.status_flag = True
-                        self.drive_pub.publish(self.wf_ackermann_msg)
+                    if not self.brake_flag:
                         
-                    else:
-                        if self.status_flag:
-                            rospy.loginfo("MANUAL MODE")
-                            self.status_flag = False
-                        self.drive_pub.publish(self.joy_ackermann_msg)
+                        if self.auto_flag == 1:  #right trigger to change modes between auto and manual
+                            if not self.status_flag:
+                                rospy.loginfo("AUTO MODE")
+                                self.status_flag = True
+                            self.drive_pub.publish(self.wf_ackermann_msg)
+                            
+                        else:
+                            if self.status_flag:
+                                rospy.loginfo("MANUAL MODE")
+                                self.status_flag = False
+                            self.drive_pub.publish(self.joy_ackermann_msg)
 
-                else:
-                    rospy.loginfo("EMERGENCY BRAKES ENABLED!!")
-                    self.brake_msg_.drive.speed = 0.0
-                    self.drive_pub.publish(self.brake_msg_)
-                    break
+                    else:
+                        rospy.loginfo("EMERGENCY BRAKES ENABLED!!")
+                        self.brake_msg_.drive.speed = 0.0
+                        self.brake_msg_.drive.steering_angle = 0.0
+                        self.drive_pub.publish(self.brake_msg_)
+                        self.en_car_status = 0   #deactivate car after ebrake, to enable hold left and right button
+                        
+
+            elif not self.en_car_status and self.en_print_flag == "ENABLED":
+                #print(self.en_print_flag)
+                rospy.loginfo("CAR DISABLED!")
+                self.en_print_flag = "DISABLED"
+
 
 
             self.rate.sleep()
